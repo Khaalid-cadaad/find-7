c5cb23c32f47aa0f7dccb25028d5f357c21d1927
unknown <khalidabdi3101@gmail.com>
2024-09-26 05:06:43 +0300

    Done find-7 exercise

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // Good Luck 💪🏾
+function findSeven(arr) {
+    // Convert the array to a single string and check for '7'
+    const hasSeven = arr.some(num => num.toString().includes('7'));
+    
+    // Return the appropriate message
+    return hasSeven ? "Found!" : "there is no 7 in the array";
+}
+
+// Examples
+console.log(findSeven([1, 2, 3, 4, 5, 6, 7]));   // "Found!"
+console.log(findSeven([8, 6, 33, 100]));         // "there is no 7 in the array"
+console.log(findSeven([2, 55, 60, 9, 86]));     // "there is no 7 in the array"
